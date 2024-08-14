74bca00ebaa89ca04a89b6d85bffa9b2ced5758b
Hoang Duc <minhduchoang2303@gmail.com>
2024-08-14 18:23:33 +0700

    first commit

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { build, defineConfig, optimizeDeps } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext'
+    }
+  },
+  build: {
+    target: 'esnext'
+  }
+})
